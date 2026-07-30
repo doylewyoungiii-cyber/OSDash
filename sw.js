3eb5ab44b5479ff040cf6f3ac1c90cadc2243da2
doylewyoungiii-cyber <doylewyoungiii@gmail.com>
2026-07-30 10:55:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xtl-os-v12';
+const CACHE = 'xtl-os-v15';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
